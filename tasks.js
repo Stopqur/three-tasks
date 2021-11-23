df2b20ad520ec3fcbb7f24e53c12297aeee87782
Stopqur <dimyachmencev@gamil.ru>
2021-11-24 02:15:20 +0300

    edit 1 and 2 tasks

--- tasks.js
+++ tasks.js
@@ -2,39 +2,65 @@
 
 //    №1
 
-const arr = [0, 1, 1]
-
+const arr = [0, 1]
 function fibonachi(len) {
-    if (arr.length !== len) {
-        arr.push(fibonachi(len - 1) + arr[arr.length - 2]) 
-        return arr[arr.length - 1]
-    } return arr[len - 1]
+    if (len < 2) {
+        console.log('Введите значение, больше 2')
+    } else {
+        if (arr.length !== len) {      
+            arr.push(fibonachi(len - 1) + arr[arr.length - 2]) 
+            return arr[arr.length - 1]
+        } else {
+            return arr[len - 1]
+        }
+    }
 }
 
-// fibonachi(5)
+// fibonachi(6)
+// console.log(arr)
 
+
+// one more function to 1 task, but without recursion
+function createFibonachi(len) {
+    const arr = [0, 1]
+    for (let i = 2; i < len; i++) {
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+    }
+    console.log(arr)
+}
+
+createFibonachi(5)
 
 
 //    №2
 
-const allStr = ['hello', 'nero', 'no', 'iStanbul', 'iodel']
+const allStr = ['hello', 'hero', 'po', 'iStanbul', 'iodel']
 
 function findSameStr(strArray) {
-    strArray = strArray
-    .map(item => item[0] + item[item.length - 1])
-    .map((item, index, array) => (index !== array[array.length - 1]) ? item === array[index + 1] : null)
+    // strArray = strArray
+    // .map(item => item[0] + item[item.length - 1])
+    // .map((item, index, array) => (index !== array.length - 1) ? item === array[index + 1] : null)
+    // strArray.pop()
+    // console.log(strArray)
+    strArray = strArray.map((item, index, arr) => {
+        if(index !== arr.length - 1) {
+            let nextStr = arr[index + 1]
+            return (item[0] + item[item.length - 1]) === nextStr[0] + nextStr[nextStr.length - 1]
+        } else {
+             return null
+        }
+    })
     strArray.pop()
     console.log(strArray)
 }
 
-// findSameStr(allStr)
+findSameStr(allStr)
 
 
 
 //    №3
 
-
-function matrix(num) {
+function createMatrix(num) {
     let flag = 1
     let arr = [[]]
 
@@ -75,4 +101,4 @@ function matrix(num) {
     console.log(arr)
 }
 
-// matrix(10)
+// createMatrix(10)
